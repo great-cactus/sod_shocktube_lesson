28c488af444fc9cbcd34a27bba7ccf31b1908622
great-cactus <akira.tsunoda.s5@dc.tohoku.ac.jp>
2026-03-31 21:15:47 +0900

    feat: add Runge-Kutta module with Butcher tableau
    RK2, SSPRK2, RK4, SSPRK3 の4スキームを Butcher tableau で定義し,
    汎用 rk_step 関数で任意の陽的 RK 法を実行可能にした.

---
+++ runge-kutta.jl
@@ -0,0 +1,72 @@
+module RungeKutta
+
+struct ButcherTableau{T<:Real}
+    A::Matrix{T}
+    b::Vector{T}
+    c::Vector{T}
+end
+
+function RK2(T=Float64)
+    ButcherTableau{T}(
+        [0 0;
+        1//2 0],
+        [0, 1],
+        [0, 1//2]
+    )
+end
+
+function SSPRK2(T=Float64)
+    ButcherTableau{T}(
+        [0 0;
+        1 0],
+        [1//2, 1//2],
+        [0, 1]
+    )
+end
+
+function RK4(T=Float64)
+    ButcherTableau{T}(
+        [0 0 0 0;
+        1//2 0 0 0;
+        0 1//2 0 0;
+        0 0 1 0],
+        [1//6, 1//3, 1//3, 1//6],
+        [0, 1//2, 1//2, 1]
+    )
+end
+
+function SSPRK3(T=Float64)
+    ButcherTableau{T}(
+        [0 0 0;
+        1 0 0;
+        1//4 1//4 0],
+        [1//6, 1//6, 2//3],
+        [0, 1, 1//2]
+    )
+end
+
+function nstages(tab::ButcherTableau)
+    length(tab.b)
+end
+
+function rk_step(f, t, u, dt, tab::ButcherTableau)
+    s = nstages(tab)
+    k = Vector{typeof(u)}(undef, s)
+
+    for i in 1:s
+        du = zero(u)
+        for j in 1:i-1
+            du += tab.A[i, j] * k[j]
+        end
+        k[i] = f(t + tab.c[i] * dt, u + dt * du)
+    end
+
+    u_new = copy(u)
+    for i in 1:s
+        u_new += dt * tab.b[i] * k[i]
+    end
+
+    return u_new
+end
+
+end # module
